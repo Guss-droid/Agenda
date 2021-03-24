631574cd9a88c5131f4a088a7fdd3da62e24aad2
Guss-droid <gustavo.re@inforoute.com.br>
2021-03-24 13:47:55 -0300

    Api 1

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Table from './pages/table';
 import Cancel from './pages/Cancelar';
+import Tempo from './pages/Api/index';
 
 export default function Routes() {
 
@@ -13,6 +14,7 @@ export default function Routes() {
                 <Route exact path="/" component={Main} />
                 <Route path="/Tabela" component={Table} />
                 <Route path="/Cancelar" component={Cancel} />
+                <Route path="/Api/Tempo" component={Tempo} />
             </Switch>
         </BrowserRouter>
     )
